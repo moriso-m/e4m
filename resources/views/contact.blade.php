a80be4ab76e25280bd5d1c25d43fb376e5db581d
moriso <injectsql001@gmail.com>
2021-02-08 10:12:48 +0300

    image upload change

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -73,6 +73,6 @@
 
 @push('scripts')
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
-<script type="text/javascript" src="js/gmaps.js"></script>
-<script src="js/contact.js"></script>
+<script type="text/javascript" src="/js/gmaps.js"></script>
+<script src="/js/contact.js"></script>
 @endpush
